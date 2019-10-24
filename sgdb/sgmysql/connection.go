9b69c7a8beedc2fe3410c748c6b3b5d076f8ca96
youai <royalchen@royalchen.com>
2019-10-24 20:55:23 +0800

    db ping

--- sgdb/sgmysql/connection.go
+++ sgdb/sgmysql/connection.go
@@ -1,12 +1,21 @@
 package sgmysql
 
 import (
+	"github.com/coderguang/GameEngine_go/sgthread"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
 func Open(cfg *MySQLCfg) (*gorm.DB, error) {
 	db, err := gorm.Open("mysql", getDbURL(cfg))
+	if err == nil {
+		go func(dbex *gorm.DB) {
+			for {
+				dbex.DB().Ping()
+				sgthread.SleepBySecond(60 * 60)
+			}
+		}(db)
+	}
 	return db, err
 }
 
