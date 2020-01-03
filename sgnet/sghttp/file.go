81ef3f64fbe30d7887ad3a6dd5aa3e0f389c0a29
youai <royalchen@royalchen.com>
2020-01-03 15:58:40 +0800

    del unuse fun

--- sgnet/sghttp/file.go
+++ sgnet/sghttp/file.go
@@ -2,7 +2,6 @@ package sghttp
 
 import (
 	"errors"
-	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 )
@@ -14,28 +13,6 @@ func CheckFileMaxSize(w http.ResponseWriter, r *http.Request, maxSize int64) err
 		return errors.New("FILE_TO_BIG")
 	}
 	return nil
-}
-
-func CheckIsAllowForm(w http.ResponseWriter, r *http.Request, filelist []string) (multipart.File, string, error) {
-	for _, v := range filelist {
-		file, _, err := r.FormFile(v)
-		if err == nil {
-			return file, v, nil
-		}
-	}
-	return nil, "", errors.New("INVALID_FILE_UPLOAD no support upload file form")
-}
-
-func CheckFileTypeMatch(w http.ResponseWriter, r *http.Request, fileType string, file multipart.File) ([]byte, error) {
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return fileBytes, err
-	}
-	detectedFileType := http.DetectContentType(fileBytes)
-	if detectedFileType != fileType {
-		return fileBytes, errors.New("INVALID_FILE_TYPE not match with" + fileType)
-	}
-	return fileBytes, nil
 }
 
 func GetFileDetectContentType(file multipart.File) (string, error) {
