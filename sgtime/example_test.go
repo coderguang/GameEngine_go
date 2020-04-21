5000f2214322eab4bcea663aa0f6578704eecccd
youai <royalchen@royalchen.com>
2020-04-21 16:49:15 +0800

    time day month week reset

--- sgtime/example_test.go
+++ sgtime/example_test.go
@@ -3,10 +3,109 @@ package sgtime
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_NextUpdate(t *testing.T) {
 	now := New()
-	fmt.Println(",now is:", GetTotalSecond(now))
+	nowsec := GetTotalSecond(now)
+	nextSec := GetNextDay(nowsec, 0, 0, 0)
+	fmt.Println("1,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("1next day is :", nextSec, ",format:", time.Unix(nextSec, 0))
+
+	nowsec = 1587571299
+	nextSec = GetNextDay(nowsec, 0, 0, 0)
+	fmt.Println("2,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("2next day is :", nextSec, ",format:", time.Unix(nextSec, 0))
+
+	nowsec = GetTotalSecond(now)
+	nextSec = GetNextDay(nowsec, 2, 0, 0)
+	fmt.Println("3,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("3next day is :", nextSec, ",format:", time.Unix(nextSec, 0))
+
+	nextSec = GetNextDay(nowsec, 1, 30, 59)
+	fmt.Println("4,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("4next day is :", nextSec, ",format:", time.Unix(nextSec, 0))
+
+	nextSec = GetNextDay(nowsec, 23, 59, 59)
+	fmt.Println("5,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("5next day is :", nextSec, ",format:", time.Unix(nextSec, 0))
+
+	t.Log("test time ok")
+}
+
+func Test_NextMonthUpdate(t *testing.T) {
+	now := New()
+	nowsec := GetTotalSecond(now)
+	nextSec := GetNextMonth(nowsec, 1, 0, 0, 0)
+	fmt.Println("1,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("1next month is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+	nowsec = 1577881500
+	nextSec = GetNextMonth(nowsec, 1, 0, 0, 0)
+	fmt.Println("2,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("2next month is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nowsec = 1577805900
+	nextSec = GetNextMonth(nowsec, 1, 0, 0, 0)
+	fmt.Println("3,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("3next month is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nowsec = 1609428300
+	nextSec = GetNextMonth(nowsec, 1, 0, 0, 0)
+	fmt.Println("4,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("4next month is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	GetTotalSecond(now)
+	nextSec = GetNextMonth(nowsec, 15, 23, 59, 59)
+	fmt.Println("5,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("5next month is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	t.Log("test time ok")
+}
+
+func Test_NextWeek(t *testing.T) {
+	now := New()
+	nowsec := GetTotalSecond(now)
+	nowsec = 1587905820
+	nextSec := GetNextWeekDay(nowsec, SG_WEEKDAY_MONDAY, 0, 0, 0)
+	fmt.Println("1,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("1next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nextSec = GetNextWeekDay(nowsec, SG_WEEKDAY_TUESDAY, 1, 0, 0)
+	fmt.Println("2,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("2next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nextSec = GetNextWeekDay(nowsec, SG_WEEKDAY_WEDNESDAY, 0, 0, 2)
+	fmt.Println("3,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("3next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nextSec = GetNextWeekDay(nowsec, SG_WEEKDAY_THURSDAY, 0, 3, 0)
+	fmt.Println("4,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("4next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nextSec = GetNextWeekDay(nowsec, SG_WEEKDAY_FRIDAY, 4, 0, 0)
+	fmt.Println("5,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("5next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nextSec = GetNextWeekDay(nowsec, SG_WEEKDAY_SATURDAY, 23, 0, 0)
+	fmt.Println("6,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("6next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
+	nextSec = GetNextWeekDay(nowsec, SG_WEEKDAY_SUNDAY, 20, 58, 30)
+	fmt.Println("7,now is:", nowsec, ",format:", time.Unix(nowsec, 0))
+	fmt.Println("7next week is :", nextSec, ",format:", time.Unix(nextSec, 0))
+	fmt.Println("=======end===\n ")
+
 	t.Log("test time ok")
 }
--- sgtime/time.go
+++ sgtime/time.go
@@ -1,6 +1,7 @@
 package sgtime
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -27,6 +28,16 @@ const (
 	FORMAT_TIME_SECOND          = "05"
 	FORMAT_TIME_YEAR_MONTH      = "200601"
 	FORMAT_TIME_YEAR_MONTH_DAY  = "20060102"
+)
+
+const (
+	SG_WEEKDAY_MONDAY    int = 0
+	SG_WEEKDAY_TUESDAY   int = 1
+	SG_WEEKDAY_WEDNESDAY int = 2
+	SG_WEEKDAY_THURSDAY  int = 3
+	SG_WEEKDAY_FRIDAY    int = 4
+	SG_WEEKDAY_SATURDAY  int = 5
+	SG_WEEKDAY_SUNDAY    int = 6
 )
 
 type DateTime = time.Time
@@ -127,6 +138,83 @@ func GetNextDay(now int64, hour int, min int, sec int) int64 {
 		nowDt := New()
 		now = GetTotalSecond(nowDt)
 	}
+	curDt := time.Unix(now, 0)
+	curZero := time.Date(curDt.Year(), curDt.Month(), curDt.Day(), 0, 0, 0, 0, curDt.Location())
+	return GetTotalSecond(TransfromTimeToDateTime(curZero)) + int64(ONE_DAY_SECOND+hour*ONE_HOUR_SECOND+min*60+sec)
+}
 
+func GetNextMonth(now int64, day int, hour int, min int, sec int) int64 {
+	if 0 == now {
+		nowDt := New()
+		now = GetTotalSecond(nowDt)
+	}
+	curDt := time.Unix(now, 0)
+	curZero := time.Date(curDt.Year(), curDt.Month(), 1, 0, 0, 0, 0, curDt.Location())
+	fmt.Println("curZero", curZero)
+	curZero = curZero.AddDate(0, 1, day-1)
+	fmt.Println("curZero2", curZero)
+	return GetTotalSecond(TransfromTimeToDateTime(curZero)) + int64(hour*ONE_HOUR_SECOND+min*60+sec)
+}
+
+func GetNextWeek(now int64, weekday int, hour int, min int, sec int) int64 {
+	if 0 == now {
+		nowDt := New()
+		now = GetTotalSecond(nowDt)
+	}
 	return 0
+}
+
+/*
+time.Weekday类型可以做运算，强制转int,会得到偏差数。
+默认是 Sunday 开始到 Saturday 算 0,1,2,3,4,5,6
+
+所以只有Monday减去Sunday的时候是正数，特殊处理下就可以了。
+*/
+func GetWeekDay(now int64) int {
+	if 0 == now {
+		nowDt := New()
+		now = GetTotalSecond(nowDt)
+	}
+	curDt := time.Unix(now, 0)
+	offset := int(curDt.Weekday())
+	if offset == 0 {
+		offset = SG_WEEKDAY_SUNDAY
+	} else {
+		offset -= 1
+	}
+	return offset
+}
+
+func GetNextWeekDay(now int64, weekday int, hour int, min int, sec int) int64 {
+	if 0 == now {
+		nowDt := New()
+		now = GetTotalSecond(nowDt)
+	}
+	curWeekDay := GetWeekDay(now)
+	timeInNextWeek := true
+	if curWeekDay < weekday {
+		//同一周
+		timeInNextWeek = false
+	} else if curWeekDay == weekday {
+		//同一天
+		nowTime := time.Unix(now, 0)
+		if nowTime.Hour() < hour {
+			timeInNextWeek = false
+		} else if hour == nowTime.Hour() {
+			if nowTime.Minute() < min {
+				timeInNextWeek = false
+			} else if min == nowTime.Minute() {
+				if nowTime.Second() < sec {
+					timeInNextWeek = false
+				}
+			}
+		}
+	}
+	todayZero := GetNextDay(now, 0, 0, 0) - ONE_DAY_SECOND
+	mondyZero := todayZero - int64(curWeekDay*ONE_DAY_SECOND)
+	if timeInNextWeek {
+		return mondyZero + 7*ONE_DAY_SECOND + int64(weekday*ONE_DAY_SECOND) + int64(hour*ONE_HOUR_SECOND+min*60+sec)
+	} else {
+		return mondyZero + int64(weekday*ONE_DAY_SECOND) + int64(hour*ONE_HOUR_SECOND+min*60+sec)
+	}
 }
